9ffe931ab533bb6f94f222fa14b39a696fdd2a89
Joetib <otiboatengjoe@gmail.com>
2020-05-08 13:07:01 -0700

    updated admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SiteConfiguration, SingleOrder, Order, UniqueFeature, Size, Color
+
+
+admin.site.register(Size)
+admin.site.register(Color)
+admin.site.register(SiteConfiguration)
+admin.site.register(UniqueFeature)
